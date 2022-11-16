e2e0ba86c442b5fd8373a0d2324f5c9655ad15b2
Thomas Bockhorn <historian.thomas@gmail.com>
2022-11-16 05:46:38 -0600

    Added some content to Contact page

--- jimstudio/resources/js/Pages/Contact.vue
+++ jimstudio/resources/js/Pages/Contact.vue
@@ -1,6 +1,25 @@
 <template>
     <main-layout>
-        Contact Page
+        <div>
+            <h4 class = "mb-5 text-2xl font-bold">Purchasing work from Jim Felder:</h4>
+            <p class = "text-xl">
+                The paintings on this site are classified as Available (work that is for sale) in the
+                notes that accompany each image. If you are interested an available work, please let me
+                know of your
+                interest in case I will be revisiting the subject at some time in the future or I have
+                plans to produce
+                a similar work.</p>
+            <p class = "text-xl pr-20">
+                If you are interested in purchasing Available work, contact me for price and details.
+                I will be happy to discuss any aspect of my work with you at any time regardless of
+                your intent
+                to purchase.
+            </p>
+            <address class = "mt-5">
+                <p class = "text-xl">Phone/text: 256.426.0558</p>
+                <p class = "text-xl">Email: jim.felder@gmail.com</p>
+            </address>
+        </div>
     </main-layout>
 </template>
 

797be540778d8438bd3e0186ef627d22d2368b0b
Thomas Bockhorn <historian.thomas@gmail.com>
2022-12-11 16:16:28 -0600

    Added a footer to the backend
    Signed-off-by: Thomas Bockhorn <historian.thomas@gmail.com>

--- jimstudio/resources/js/Layouts/AuthenticatedLayout.vue
+++ jimstudio/resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,14 +1,18 @@
 <template>
     <backend-nav></backend-nav>
     <slot/>
+    <vue-footer></vue-footer>
 </template>
 <script>
 import backendNav from "../Components/backend/navbar/vue-backend-nav.vue";
+import footer from "../Components/frontend/footer/vue-footer.vue";
 
-export default{
+
+export default {
     name: "AuthenticatedLayout",
-    components:{
+    components: {
         "backend-nav": backendNav,
+        "vue-footer": footer
     }
 }
 </script>

72906bc296295474948f57e9328483322f889a36
Thomas Bockhorn <historian.thomas@gmail.com>
2022-12-11 15:52:36 -0600

    Only one user can register.  If more than one is registered, then the visitor is redirected to the welcome page.
    Signed-off-by: Thomas Bockhorn <historian.thomas@gmail.com>

--- jimstudio/app/Http/Controllers/Auth/RegisteredUserController.php
+++ jimstudio/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,43 +3,62 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Painting;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\Rules;
+use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class RegisteredUserController extends Controller
 {
     /**
      * Display the registration view.
      *
-     * @return \Inertia\Response
+     * @return Response
      */
-    public function create()
+    public function create(): Response
     {
-        return Inertia::render('Auth/Register');
+        //If there is more than one user, then the registration page is not available
+        if (User::count() < 1) {
+            return Inertia::render('Auth/Register');
+        }
+        //The unregistered user is redirected to welcome page
+        return Inertia::render('Welcome', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'paintings' => Painting::all()
+        ]);
     }
 
     /**
      * Handle an incoming registration request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
+     * @param Request $request
      *
-     * @throws \Illuminate\Validation\ValidationException
+     * @return Application|RedirectResponse|Redirector
+     *
+     * @throws ValidationException
      */
-    public function store(Request $request)
+    public function store(Request $request): Redirector|RedirectResponse|Application
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:'.User::class,
+            'email' => 'required|string|email|max:255|unique:' . User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+
+        //After checking if there is only one user in the database, it will allow the user to register
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
@@ -48,8 +67,11 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
+
         Auth::login($user);
 
         return redirect(RouteServiceProvider::HOME);
+
+
     }
 }
